dfb363b401d8c58eaf1070eeeb4ecdb457be9491
Daniel Rigoni <daniel.rigoni@luizalabs.com>
2021-01-08 22:00:53 -0300

    Add new envs from retry

--- src/main/java/com/challenge/luizalabs/service/ProductService.java
+++ src/main/java/com/challenge/luizalabs/service/ProductService.java
@@ -1,8 +1,8 @@
 package com.challenge.luizalabs.service;
 
-import com.challenge.luizalabs.dto.ProductDto;
 import com.challenge.luizalabs.exception.InternalServerErrorException;
 import com.challenge.luizalabs.service.api.ApiChallengeService;
+import com.challenge.luizalabs.v1.dto.ProductDto;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
 
@@ -11,20 +11,33 @@ import java.util.UUID;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
+  @Value("${api.retry.maxAttempts}")
+  private int maxAttempts;
+
+  @Value("${api.retry.waitDurationMilliseconds}")
+  private long waitDuration;
+
   @Autowired
   private ApiChallengeService apiChallengeService;
 
+  /**
+   * Method responsible to get a product by id calling a api (this method has a retry).
+   *
+   * @param productId {UUID}
+   * @return ProductDto
+   */
   public ProductDto getProductApi(final UUID productId) {
     try {
       Supplier<ProductDto> productDtoCheckedFunction0;
       RetryConfig retryConfig = RetryConfig.custom()
-          .maxAttempts(3)
-          .waitDuration(Duration.ofMillis(5000))
+          .maxAttempts(maxAttempts)
+          .waitDuration(Duration.ofMillis(waitDuration))
           .retryOnException(t -> t instanceof InternalServerErrorException)
           .build();
       Retry retry = Retry.of("retry-call-api-challenge", retryConfig);
